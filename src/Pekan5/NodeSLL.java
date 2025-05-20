c18c3c59e5765dd5321594f0cf6d420b8d5fd541
mandaw34 <pdevinaamanda@gmail.com>
2025-05-20 15:34:11 +0700

    Praktikum Pekan 5

---
+++ src/Pekan5/NodeSLL.java
@@ -0,0 +1,15 @@
+package Pekan5;
+
+public class NodeSLL {
+	//node bagian data
+	int data;
+	//pointer ke node berikutnya
+	NodeSLL next;
+	//kontruktor menginisialisasikan node dengan data
+	public NodeSLL(int data)
+	{
+	this.data = data;
+	this.next = null;
+	}
+
+}
